06652d92f2f11986127e8b0678015c1e43852f7f
K SRI SAI MANI <srisaimanikovvada@gmail.com>
2022-01-22 19:14:24 +0530

    Update Area.py

--- Area.py
+++ Area.py
@@ -6,3 +6,9 @@ print("The area of circle = ",area)
 
 #OUTPUT:
 #The area of circle =  314.52
+
+#SECOND WAY OF INPUTTING 
+PI = 3.14
+r = float(input("Enter the radius of a circle:"))
+area = PI * r * r
+print("Area of a circle = %.2f" %area)
