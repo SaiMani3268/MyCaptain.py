f1369883e6be120fc0638b588c14bc6462c1e994
K SRI SAI MANI <srisaimanikovvada@gmail.com>
2022-01-24 08:41:49 -0800

    Update Area.py

--- Area.py
+++ Area.py
@@ -5,8 +5,8 @@ area=pie*radius*radius
 print("The area of circle = ",area)
 
 #OUTPUT:
-Enter radius of the circle: 10
-The area of circle =  314.52
+#Enter radius of the circle: 10
+#The area of circle =  314.52
 
 #Extention 
 fun_name = input("Enter the filename:")
