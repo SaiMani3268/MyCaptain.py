950cd17515abca1d6edbadf4fa8fa9087ed0890e
K SRI SAI MANI <srisaimanikovvada@gmail.com>
2022-01-24 22:00:05 +0530

    Add files via upload

---
+++ Area.py
@@ -0,0 +1,21 @@
+#AREA OF CIRCLE
+radius=10
+pie=3.1452
+area=pie*radius*radius
+print("The area of circle = ",area)
+
+#OUTPUT:
+The area of circle =  314.52
+
+#Extention 
+fun_name = input("Enter the filename:")
+x = fun_name.split(".")
+print("The extention of the file is:" + x[-1])
+
+#OUTPUT-1:
+#Enter the filename:area.py
+#The extention of the file is: py
+
+#OUTPUT-2:
+#Enter the filename:area.java
+#The extention of the file is: java
